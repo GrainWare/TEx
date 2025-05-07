096ed3dd3e739b635a426e191e3adf239178fa33
electron271 <66094410+electron271@users.noreply.github.com>
2025-05-07 13:03:23 -0500

    simple (yet overkill) homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { Canvas } from '@threlte/core';
+  import Scene from './Scene.svelte';
+</script>
+
+<div class="fixed inset-0 overflow-hidden">
+  <div class="w-full h-full">
+    <Canvas>
+      <Scene />
+    </Canvas>
+  </div>
+</div>
+
+<!-- normally hidden links for screen reader who cant see the 3d scene -->
+<a href="/about" class="sr-only">About</a>
